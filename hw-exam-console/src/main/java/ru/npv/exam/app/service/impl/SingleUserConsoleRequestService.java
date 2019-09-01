b850db38d2e8280de229dc9c4971b69738913282
Павел <netcpv22@mail.ru>
2019-09-01 20:18:28 +0300

    немного в сервис опроса

--- hw-exam-console/src/main/java/ru/npv/exam/app/service/impl/SingleUserConsoleRequestService.java
+++ hw-exam-console/src/main/java/ru/npv/exam/app/service/impl/SingleUserConsoleRequestService.java
@@ -10,21 +10,32 @@ import ru.npv.exam.app.service.UserRequestService;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class SingleUserConsoleRequestService implements UserRequestService<InputStream, OutputStream> {
-    private final Logger LOG = LoggerFactory.getLogger(SingleUserConsoleRequestService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SingleUserConsoleRequestService.class);
+    private static final String PROPERTY_MAX_QUESTIONS = "max.questions";
 
     private final LoadQuestionsService questionsService;
     private final StringPropertiesService propertiesService;
 
     private Process process;
+    private Integer maxQuestions;
 
 
     public SingleUserConsoleRequestService(LoadQuestionsService questionsService, StringPropertiesService propertiesService) {
         this.questionsService = questionsService;
         this.propertiesService = propertiesService;
+        if (propertiesService != null) {
+            try {
+                maxQuestions = Integer.valueOf(propertiesService.getStringProperty(PROPERTY_MAX_QUESTIONS).get());
+            } catch (Exception e) {
+                LOG.debug("Свойство {} не получено", PROPERTY_MAX_QUESTIONS);
+            }
+        }
     }
 
     @Override
@@ -33,8 +44,8 @@ public class SingleUserConsoleRequestService implements UserRequestService<Input
     }
 
     private void restartProcess(InputStream input, OutputStream output) {
-        process = new Process(questionsService.load(), input, output);
-
+        process = new Process(questionsService.load(), input, output, maxQuestions);
+        process.start();
     }
 
     @Override
@@ -49,25 +60,52 @@ public class SingleUserConsoleRequestService implements UserRequestService<Input
     }
 
     class Process {
+        private final Integer DEFAULT_MAX_QUESTIONS = 5;
+
         private final List<AbstractQuestion> questions;
         private final List<Integer> availableIndexes;
         private final InputStream consoleInputStream;
         private final OutputStream consoleOutputStream;
+        private final Random random;
+
+        private int questionsCount;
         @Getter
         private AbstractQuestion currentQuestion;
+        @Getter
+        private int maxQuestions;
 
-        Process(List<AbstractQuestion> questions, InputStream consoleInputStream, OutputStream consoleOutputStream) {
+        Process(List<AbstractQuestion> questions, InputStream consoleInputStream, OutputStream consoleOutputStream, Integer maxQuestions) {
             this.questions = questions;
             this.consoleInputStream = consoleInputStream;
             this.consoleOutputStream = consoleOutputStream;
-            availableIndexes = new LinkedList<>();
+            this.availableIndexes = new LinkedList<>();
             for (int i=0; i<=questions.size(); i++) {
-                availableIndexes.add(i);
+                this.availableIndexes.add(i);
             }
+            this.random = new Random(new Date().getTime());
+            this.questionsCount = 0;
+            this.maxQuestions = maxQuestions!=null && maxQuestions>0 ? maxQuestions : DEFAULT_MAX_QUESTIONS;
+        }
+
+        public void start() {
+            // Начать задавать вопросы
         }
 
         public void nextQuestion() {
+            if (questionsCount >= maxQuestions) {
+                stop();     // достаточно
+            }
+            if (availableIndexes.isEmpty()) {
+                stop();     // закончились
+            }
+            int qindex = random.nextInt(availableIndexes.size()-1);
+            currentQuestion = questions.get(qindex);
+            availableIndexes.remove(qindex);
+            questionsCount++;
+        }
 
+        private void stop() {
+            // Перестать задавать вопросы
         }
     }
 }
