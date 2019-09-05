255c7dfea9ad2dd96ad3513312cffbc213170d38
Павел <netcpv22@mail.ru>
2019-09-05 22:43:49 +0300

    снова замена контекста

--- hw-exam-console-javaconfig/src/main/java/ru/npv/exam/jc/app/Application.java
+++ hw-exam-console-javaconfig/src/main/java/ru/npv/exam/jc/app/Application.java
@@ -1,13 +1,17 @@
 package ru.npv.exam.jc.app;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import ru.npv.exam.jc.app.domain.app.ExamProcess;
 import ru.npv.exam.jc.app.domain.app.UserRequestService;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
+@ComponentScan(basePackages = "ru.npv.exam.jc.app.service")
+@PropertySource(value = "classpath:settings.properties")
 public class Application {
 
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class Application {
     }
 
     public void run() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         UserRequestService<InputStream, OutputStream> requestService = context.getBean(UserRequestService.class);
         ExamProcess process = requestService.getExamProcess(System.in, System.out);
         process.run();
