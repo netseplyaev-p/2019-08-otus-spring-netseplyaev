b80b9d663fe8c795dd2f63806a762dcfaefbbcc8
Павел <netcpv22@mail.ru>
2019-09-05 22:39:09 +0300

    обратная замена для мержа

--- hw-exam-console-javaconfig/src/main/java/ru/npv/exam/jc/app/Application.java
+++ hw-exam-console-javaconfig/src/main/java/ru/npv/exam/jc/app/Application.java
@@ -1,17 +1,13 @@
 package ru.npv.exam.jc.app;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.npv.exam.jc.app.domain.app.ExamProcess;
 import ru.npv.exam.jc.app.domain.app.UserRequestService;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 
-@ComponentScan(basePackages = "ru.npv.exam.jc.app.service")
-@PropertySource(value = "classpath:settings.properties")
 public class Application {
 
     public static void main(String[] args) {
@@ -19,7 +15,7 @@ public class Application {
     }
 
     public void run() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(this.getClass());
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         UserRequestService<InputStream, OutputStream> requestService = context.getBean(UserRequestService.class);
         ExamProcess process = requestService.getExamProcess(System.in, System.out);
         process.run();
